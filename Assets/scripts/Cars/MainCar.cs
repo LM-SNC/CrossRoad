9dc3eec5441609693709ae555f5cf7756f720ca5
ART <wazilinik@gmail.com>
2020-02-05 21:08:01 +0300

    MainCar

---
+++ Assets/scripts/Cars/MainCar.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCar : MonoBehaviour
+{
+    public float speed = 1;
+    public float speedup = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speedup = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //moove
+        transform.Translate(new Vector3(speed * Input.GetAxis("Horizontal") * Time.deltaTime,speedup * Time.deltaTime),0);
+
+        if (Input.GetKey(KeyCode.W) && speedup < 2.5f)
+        {
+            speedup += 0.01f;
+        }
+        else if(speedup > 0)
+        {
+            speedup -= 0.02f;
+        }
+
+        if (Input.GetKey(KeyCode.S) && speedup > -3.5f)
+        {
+            speedup -= 0.009f;
+        }
+        else if(speedup < 0)
+        {
+            speedup += 0.02f;
+        }
+        //moove
+    }
+  
+
+        
+}
